14970f009f9189eb45aecaeda598bcf0eecd1a45
eminabesic <besic.e17@gmail.com>
2024-05-25 22:08:42 +0200

    dodane forme i lokacija

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from .config import get_database_url
 
 engine = create_engine("postgresql://" + get_database_url())
-SessionLocal = sessionmaker(autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
